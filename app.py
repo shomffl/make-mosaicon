11e3050210f46c155eca1d7728c8f393c7323560
shomffl <showatanabe0226@gmail.com>
2021-10-08 00:39:19 +0900

    エラーの原因調査

--- app.py
+++ app.py
@@ -89,30 +89,30 @@ def upload():
         split_filename = save_filename.split('.')
         send_filename = split_filename[0]
 
-        filename = f"{download_file_path}/resize_image.png"
+        # filename = f"{download_file_path}/resize_image.png"
 
-        if make_course == True:
-            split_image = SplitOriginal(8, filename,"./frontend/build/static/images/split_original_files")
-            split_image.split_image()
-            read_original = GetRgb("/split_original_files")
-            read_material = GetRgb("simple_images/small_material_files")
-            cul = CompareColors(read_original.get_rgb(), read_material.get_rgb()).compare()
-            create = ConnectImage(8, 400, cul,"simple_images")
-            create.connect_image()
+        # if make_course == True:
+        #     split_image = SplitOriginal(8, filename,"./frontend/build/static/images/split_original_files")
+        #     split_image.split_image()
+        #     read_original = GetRgb("/split_original_files")
+        #     read_material = GetRgb("simple_images/small_material_files")
+        #     cul = CompareColors(read_original.get_rgb(), read_material.get_rgb()).compare()
+        #     create = ConnectImage(8, 400, cul,"simple_images")
+        #     create.connect_image()
 
-        elif make_course == False:
-            split_image = SplitOriginal(8, filename,"./frontend/build/static/images/split_original_files")
-            split_image.split_image()
-            read_original = GetRgb("/split_original_files")
-            read_material = GetRgb("fullscale_images/small_material_files")
-            cul = CompareColors(read_original.get_rgb(), read_material.get_rgb()).compare()
-            create = ConnectImage(8, 400, cul, "fullscale_images")
-            create.connect_image()
+        # elif make_course == False:
+        #     split_image = SplitOriginal(8, filename,"./frontend/build/static/images/split_original_files")
+        #     split_image.split_image()
+        #     read_original = GetRgb("/split_original_files")
+        #     read_material = GetRgb("fullscale_images/small_material_files")
+        #     cul = CompareColors(read_original.get_rgb(), read_material.get_rgb()).compare()
+        #     create = ConnectImage(8, 400, cul, "fullscale_images")
+        #     create.connect_image()
 
         randlst = [random.choice(string.ascii_letters + string.digits) for i in range(6)]
         randstr = ''.join(randlst)
 
-        cloudinary.uploader.upload(file=f"./frontend/build/static/images/download_images/mosaic_image.png", public_id=f"download_images/{send_filename}{randstr}")
+        cloudinary.uploader.upload(file=f"./frontend/build/static/images/download_images/resize_image.png", public_id=f"download_images/{send_filename}{randstr}")
 
 
         return {"image":f"https://res.cloudinary.com/shoimages/download_images/{send_filename}{randstr}.png"}
