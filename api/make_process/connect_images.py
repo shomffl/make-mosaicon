ce269b9da9dd15d9c06adfc4143df8284f6e846a
shomffl <showatanabe0226@gmail.com>
2021-10-14 21:59:19 +0900

    コメントの追加

--- api/make_process/connect_images.py
+++ api/make_process/connect_images.py
@@ -13,15 +13,18 @@ class ConnectImage():
         self.save_name = save_name
 
     def connect_image(self):
+        #一行に何個の画像を結合するのか決める
         side = self.length
         img_num = int(side / self.size)
 
-        list1 = np.array_split(self.min_numbers, img_num)
+        #一行に結合する画像の個数分でリストを分割
+        split_list = np.array_split(self.min_numbers, img_num)
 
-        big_img = [[cv2.imread(f"{self.base_path}/{self.course_name}/big_material_files/canvas{i[j]}.png")  for j in range(img_num)] for i in list1]
+        #モザイクアート作成用の画像を読み込む
+        big_img_read = [[cv2.imread(f"{self.base_path}/{self.course_name}/big_material_files/canvas{i[j]}.png")  for j in range(img_num)] for i in split_list]
 
+        #リストに格納されているファイル名の画像を結合していく
+        cul = cv2.vconcat([cv2.hconcat(i) for i in big_img_read])
 
-        cul = cv2.vconcat([cv2.hconcat(i) for i in big_img])
-
-
+        #作成したモザイクアートを指定されたフォルダに保存する
         cv2.imwrite(f"{self.base_path}/download_images/{self.save_name}", cul)
