d3cf37a4082eb569bcb1e5cae417fdc5b7e760b3
shomffl <showatanabe0226@gmail.com>
2021-10-14 10:52:00 +0900

    画像ダウンロードURLをprocess.env.PUBLIC_URLから取得

--- frontend/src/components/SubmitImageButton.jsx
+++ frontend/src/components/SubmitImageButton.jsx
@@ -34,8 +34,12 @@ export const SubmitImageButton = (props) => {
           header: { "content-type": "multipart/form-data" },
         })
         .then((result) => {
-          changeImage(`${process.env.PUBLIC_URL}/static/images/download_images/${result.data.image}`);
-          setImageUrl(result.data.image);
+          changeImage(
+            `${process.env.PUBLIC_URL}/static/images/download_images/${result.data.image}`
+          );
+          setImageUrl(
+            `${process.env.PUBLIC_URL}/static/images/download_images/${result.data.image}`
+          );
           setOpenDownload(!openDownload);
         })
         .catch((error) => {
