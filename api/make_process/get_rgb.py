b419bfe7aab242ad1e6255cd5cf6d0e61770c83a
shomffl <showatanabe0226@gmail.com>
2021-10-14 21:45:41 +0900

    コメントの変更

--- api/make_process/get_rgb.py
+++ api/make_process/get_rgb.py
@@ -11,7 +11,7 @@ class GetRgb:
         self.foldername = foldername
 
 
-    #指定されたフォルダ内の画像の画素値を取得し、一枚のタイルの画素値の平均をリストに格納する。
+    #指定されたフォルダ内の画像の画素値を取得し、一枚の画像の画素値の平均をリストに格納する。
     def get_rgb(self):
         count_file_num = len(glob.glob(f"{self.base_path}/{self.foldername}/*.png"))
         rgb_list = [cv2.imread(f"{self.base_path}/{self.foldername}/canvas{i}.png").mean(0).mean(0)
