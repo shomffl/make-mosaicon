9b1bfba1a9f96a7a4a9a7493c751af5969d00f80
shomffl <showatanabe0226@gmail.com>
2021-10-14 21:44:20 +0900

    コメントの追加

--- api/make_process/split_image.py
+++ api/make_process/split_image.py
@@ -11,13 +11,17 @@ class SplitImage:
         self.filepath = filepath
 
     def split_image(self):
+        #画像を読み込んで画像の幅を取得する
         img = cv2.imread(self.filename)
         side = img.shape[0]
+
+        #画像を一行何分割するか決定する
         img_num = int(side / self.size)
 
-        #座標を指定してオリジナルの画像を切り分けていく。
+        #分割する画像の座標を指定してオリジナルの画像を切り分けていく
         original_list = [img[(i * self.size) : ((i+1)*self.size), (j * self.size) : ((j+1) * self.size)]
                          for i in range(img_num) for j in range(img_num)]
 
+        #分割した画像をフォルダに保存する
         for num, list_num in enumerate(original_list):
             cv2.imwrite(os.path.join("{}/canvas{}.png".format(self.filepath,num)),list_num)
