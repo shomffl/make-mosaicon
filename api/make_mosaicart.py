dbdeea62f68bc538241441aa16b70cd25ddfc8ec
shomffl <showatanabe0226@gmail.com>
2021-10-14 22:31:01 +0900

    コメントの追加

--- api/make_mosaicart.py
+++ api/make_mosaicart.py
@@ -3,6 +3,7 @@ from api.make_process.compare_colors import  CompareColors
 from api.make_process.connect_images import ConnectImage
 from api.make_process.split_image import SplitImage
 
+
 class MakeMosaicon:
 
     def __init__(self, create_course, base_path, reference_filename, mosaic_filename):
@@ -12,15 +13,22 @@ class MakeMosaicon:
         self.mosaic_filename = mosaic_filename
 
     def make_mosaic(self):
+        #オリジナル画像を細かく分割する
         split_image = SplitImage(8, self.reference_filename,"./frontend/build/static/images/split_original_files")
         split_image.split_image()
 
+        #細かく分割してオリジナル画像の画素値を読み込んでリストに格納する
         read_original = GetRgb(self.base_path, "split_original_files")
-        read_material = GetRgb(self.base_path, f"{self.create_course}/small_material_files")
         original_rgb = read_original.get_rgb()
+
+        #素材画像の画素値を読み込んでリストに格納する
+        read_material = GetRgb(self.base_path, f"{self.create_course}/small_material_files")
         material_rgb = read_material.get_rgb()
+
+        #取得した画素値同士を比較して、最も差の小さい素材画像を特定する
         compare_rgb = CompareColors(original_rgb, material_rgb)
         culculate_difference = compare_rgb.compare()
 
+        #最も差の小さい素材画像同士を結合して、モザイクアートを作成する
         create_mosaic = ConnectImage(8, 400, self.base_path, culculate_difference, self.create_course, f"{self.mosaic_filename}.png")
         create_mosaic.connect_image()
